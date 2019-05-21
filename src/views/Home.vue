a799ffdfd1a89ee1eb392b5d08ea87e59ca94cff
Ahmed Akmal <ahmed@tradingspine.com>
2019-05-21 12:47:57 +0200

    v0.1.10 - Added cropped (oekotex_de.png) to (src/assets/img/certificates/oekotex/). Modified (Home.vue) by adding the certificate logo, and modified (_styles.scss) to add responsivness to the image. Restructured custom scss, by adding (scss/custom) folder, holding the (_settings.scss & _styles.scss & _utilities.scss), where _utilities.scss is wrapper for (scss/custom/utilities/) which holds all custom helpers (colors & shadows & sizing & spacing). Refactored (app.scss) according to the new structure for custom scss.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
         <section class="testpacket-header" data-interchange="[assets/img/lillydoo-testpaket-header-small.jpg, small], [assets/img/lillydoo-testpaket-header-medium.jpg, medium], [assets/img/lillydoo-testpaket-header-large.jpg, large]">
+        <img src="@/assets/img/certificates/oekotex/oekotex_de.png" alt="Oeko-Tex Certificate" class="oekotex-certificate shadow">
         </section>
     </div>
 </template>
